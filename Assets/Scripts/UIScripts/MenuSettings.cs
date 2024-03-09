df22ae29b14c1451e17f810d2a1095e7b5213ffe
duygudumlupinar <87421564+duygudumlupinar@users.noreply.github.com>
2024-03-09 20:32:11 +0300

    UI Updates

--- Assets/Scripts/UIScripts/MenuSettings.cs
+++ Assets/Scripts/UIScripts/MenuSettings.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class MenuSettings : MonoBehaviour
 {
-    public Animator transparentBgAnim, mainBgAnim, secondBgAnim, resumeButtonAnim, musicButtonAnim, mainMenuButtonAnim;
+    public Animator transparentBgAnim, mainBgAnim, secondBgAnim, restartButtonAnim, resumeButtonAnim, musicButtonAnim, mainMenuButtonAnim;
     public Button pauseButton;
 
     public void Pause()
@@ -15,6 +15,7 @@ public class MenuSettings : MonoBehaviour
         transparentBgAnim.SetBool("Paused", true);
         mainBgAnim.SetBool("Paused", true);
         secondBgAnim.SetBool("Paused", true);
+        restartButtonAnim.SetBool("Paused", true);
         resumeButtonAnim.SetBool("Paused", true);
         musicButtonAnim.SetBool("Paused", true);
         mainMenuButtonAnim.SetBool("Paused", true);
@@ -30,6 +31,7 @@ public class MenuSettings : MonoBehaviour
         transparentBgAnim.SetBool("Paused", false);
         mainBgAnim.SetBool("Paused", false);
         secondBgAnim.SetBool("Paused", false);
+        restartButtonAnim.SetBool("Paused", false);
         resumeButtonAnim.SetBool("Paused", false);
         musicButtonAnim.SetBool("Paused", false);
         mainMenuButtonAnim.SetBool("Paused", false);
@@ -37,7 +39,7 @@ public class MenuSettings : MonoBehaviour
 
     IEnumerator StopTheGame()
     {
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(1);
         Time.timeScale = 0;
     }
 }
