a8eab466ad45c23f3bbf3e99445e35bc198e6b33
duygudumlupinar <87421564+duygudumlupinar@users.noreply.github.com>
2024-03-11 02:23:35 +0300

    UI Updates

--- Assets/Scripts/UIScripts/MainMenuButton.cs
+++ Assets/Scripts/UIScripts/MainMenuButton.cs
@@ -7,6 +7,6 @@ public class MainMenuButton : MonoBehaviour
 {
     public void GoToMainMenu()
     {
-        SceneManager.LoadScene("StartScene");
+        SceneManager.LoadScene("SampleScene");
     }
 }
